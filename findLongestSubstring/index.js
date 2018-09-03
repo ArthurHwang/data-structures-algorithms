6be156abb0068ded4a532116d733c89279350154
ArthurHwang <ahwang55@gmail.com>
2018-09-03 16:39:11 -0700

    Added unit tests

--- findLongestSubstring/index.js
+++ findLongestSubstring/index.js
@@ -1,29 +1,26 @@
 const { performance } = require('perf_hooks')
 
-
 // Write a function called findLongestSubstring, which accepts
 // a string and returns the length of the longest substring with all
 // distinct characters
 
+// o(n) solution
 
+function findLongestSubstring(str) {
+  if (!str) return null
+  let longest = 0
+  let seen = {}
+  let start = 0
 
- findLongestSubstring(str) {
-    let longest = 0;
-    let seen = {};
-    let start = 0;
-
-    for (let i = 0; i < str.length; i++) {
-      let char = str[i];
-      if (seen[char]) {
-        start = Math.max(start, seen[char]);
-      }
-      // index - beginning of substring + 1 (to include current in count)
-      longest = Math.max(longest, i - start + 1);
-      // store the index of the next char so as to not double count
-      seen[char] = i + 1;
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i]
+    if (seen[char]) {
+      start = Math.max(start, seen[char])
     }
-    return longest;
+    longest = Math.max(longest, i - start + 1)
+    seen[char] = i + 1
   }
-
+  return longest
+}
 
 module.exports = findLongestSubstring
