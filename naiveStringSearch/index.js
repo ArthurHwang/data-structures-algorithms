9c3f00c12501e5ea1213c3f0c78a9b5065e3fa05
ArthurHwang <ahwang55@gmail.com>
2018-09-03 22:43:14 -0700

    Added naiveStringSearch with unit tests

---
+++ naiveStringSearch/index.js
@@ -0,0 +1,22 @@
+const { performance } = require('perf_hooks')
+
+function naiveStringSearch(str, substr) {
+  let count = 0
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < substr.length; j++) {
+      if (str[i + j] !== substr[j]) break
+      if (j === substr.length - 1) count++
+    }
+  }
+  return count
+}
+
+console.log()
+// performance
+let t1 = performance.now()
+naiveStringSearch('lomgzomgxomg', 'omg')
+let t2 = performance.now()
+console.log(`elapsed time: ${(t2 - t1) / 1000} seconds`)
+
+module.exports = naiveStringSearch
