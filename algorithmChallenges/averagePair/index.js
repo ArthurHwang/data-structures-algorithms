c1cd852e3be0291fa89b0459c07a4f6f21c768a2
ArthurHwang <ahwang55@gmail.com>
2018-09-06 20:10:27 -0700

    Cleaned up folder structure

--- algorithmChallenges/averagePair/index.js
+++ algorithmChallenges/averagePair/index.js
@@ -2,7 +2,8 @@ const { performance } = require('perf_hooks')
 
 // Write a function called averagePair.
 // Given a sorted array of integers and a target average
-// determine if there is a pair of values in the array where the average of the pair equals the target average. There may be more than one pair that matches the target averagePair
+// determine if there is a pair of values in the array where the average of the pair equals the target average.
+// There may be more than one pair that matches the target averagePair
 
 // o(n) solution
 function averagePair(arr, target) {
