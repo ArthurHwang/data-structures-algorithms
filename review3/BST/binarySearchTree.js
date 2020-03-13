e55f7bae5270d0dae081f61bdd7ca847e568c9d5
ArthurHwang <ahwang55@gmail.com>
2020-03-13 11:56:47 -0700

    add new data structures

---
+++ review3/BST/binarySearchTree.js
@@ -0,0 +1,15 @@
+class Node {
+  constructor(val) {
+    this.left = null;
+    this.right = null;
+    this.val = val;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+}
+
+const BST = new BinarySearchTree();
