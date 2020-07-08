899dfde6e727b8d95d765a7d32829406ee65e8df
ArthurHwang <ahwang55@gmail.com>
2020-07-08 11:53:58 -0700

    add LC problem

--- data-structures/graph/index.js
+++ data-structures/graph/index.js
@@ -27,16 +27,46 @@ module.exports.Graph = class {
     }
     delete this.adjacencyList[vertex];
   };
+
+  DFSRecursive = (start) => {
+    const result = [];
+    const visited = {};
+
+    const traverse = (vertex) => {
+      if (!vertex) return null;
+
+      visited[vertex] = true;
+      result.push(vertex);
+
+      this.adjacencyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          traverse(neighbor);
+        }
+      });
+    };
+
+    traverse(start);
+    return result;
+  };
 };
 
 const G = new module.exports.Graph();
 
-G.addVertex('tokyo');
-G.addVertex('usa');
-G.addVertex('taiwan');
-G.addEdge('tokyo', 'usa');
-G.addEdge('tokyo', 'taiwan');
-G.removeEdge('tokyo', 'usa');
-G.removeVertex('taiwan');
+G.addVertex('A');
+G.addVertex('B');
+G.addVertex('C');
+G.addVertex('D');
+G.addVertex('E');
+G.addVertex('F');
+
+G.addEdge('A', 'B');
+G.addEdge('A', 'C');
+G.addEdge('B', 'D');
+G.addEdge('C', 'E');
+G.addEdge('D', 'E');
+G.addEdge('D', 'F');
+G.addEdge('E', 'F');
+
+console.log(G.DFSRecursive('A'));
 
 console.log(G.adjacencyList);
