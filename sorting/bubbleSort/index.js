896a4a5a969785815720b26e5f196ece3c1f2509
ArthurHwang <ahwang55@gmail.com>
2020-07-10 15:11:56 -0700

    add insertion / bubble / selection sort

---
+++ sorting/bubbleSort/index.js
@@ -0,0 +1,21 @@
+const swap = (arr, idx1, idx2) => {
+  let temp = arr[idx1];
+  arr[idx1] = arr[idx2];
+  arr[idx2] = temp;
+};
+
+module.exports.bubbleSort = (arr) => {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    let noSwaps = true;
+    for (let j = 0; j < i; j++) {
+      console.log(arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+        noSwaps = false;
+        j = 0;
+      }
+    }
+    if (noSwaps) break;
+  }
+  return arr;
+};
